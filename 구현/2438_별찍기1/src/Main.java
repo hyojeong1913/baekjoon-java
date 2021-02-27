a1ef90f30aae701c1e4157fc9aac2cc74c7baab3
LEE HYO JEONG <hyojeong1913@naver.com>
2021-02-27 23:34:18 +0900

    백준 2438번 / 별찍기 - 1

---
+++ 구현/2438_별찍기1/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * 5
+ * *
+ * **
+ * ***
+ * ****
+ * *****
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int input = sc.nextInt();
+		
+		for (int i=0; i<input; i++) {
+
+			for (int j=0; j<=i; j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+}
